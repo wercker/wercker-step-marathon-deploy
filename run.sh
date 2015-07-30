c5a63969e38dd243ce96380cc567975b7705fc2f
Toon Verbeek <toon.verbeek1@gmail.com>
2015-07-30 12:49:22 +0200

    initial commit

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ -z $WERCKER_MARATHON_DEPLOY_APP_JSON_FILE ]; then
+  fail "You must specify a JSON file to deploy" 
+fi
+
+if [ -z $WERCKER_MARATHON_DEPLOY_APP_MARATHON_URL ]; then
+  fail "You must specify a valid marathon URL"
+fi
+
+MARATHON_ENDPOINT="$WERCKER_MARATHON_DEPLOY_MARATHON_URL/v2/apps"
+
+info "deploying $WERCKER_MARATHON_DEPLOY_APP_JSON_FILE"
+cat $WERCKER_MARATHON_DEPLOY_APP_JSON_FILE
+
+info "destroying previous instances..."
+
+if [ -z $WERCKER_MARATHON_DEPLOY_APP_NAME ]; then
+  fail "You must specify a valid app name"
+fi
+
+url -XDELETE -H "Content-type: application/json" $MARATHON_ENDPOINT/$WERCKER_MARATHON_DEPLOY_APP_NAME
+
+info "deploying new version..."
+sleep 1
+
+curl -XPOST -H "Content-type: application/json" -d @$WERCKER_MARATHON_DEPLOY_JSON_FILE $MARATHON_ENDPOINT
+
+success "Deploy successful"
